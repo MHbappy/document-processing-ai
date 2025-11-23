8b0993c6c6236f218ac30f8746cdec15beaf7213
bappy <mehedi.bappy@suffixit.com>
2025-11-23 12:18:40 +0600

    added

--- front-end-application/app/page.tsx
+++ front-end-application/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello World 🚀</h1>
+      <h1>Hello World Bangladesh</h1>
     </div>
   );
 }
